d10aee2e49820dd12780fddd9d2b66c836cee5c1
fhyxzmkh <2289960268@qq.com>
2024-10-10 19:22:12 +0800

    Finished v1.0

--- app/src/main/java/org/flappybird/GameView.java
+++ app/src/main/java/org/flappybird/GameView.java
@@ -13,12 +13,29 @@ import android.view.View;
 
 import androidx.annotation.NonNull;
 
-import java.io.ObjectStreamClass;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameView extends SurfaceView implements SurfaceHolder.Callback, Runnable {
 
+    public static final int GAME_START = 200; // 游戏开始
+    public static final int GAME_STOP = 400; // 游戏停止
+    public static final int GAME_OVER = 500; // 游戏结束
+    public static final int GAME_READY = 100; // 游戏准备
+    int obstacleInterval; // 障碍物上下间距
+    int score = 0;
+    Body collisionBody;
+    int CreateObstacleTime = 0;
+    int moveSpeed = 10;
+    boolean isDown = true;
+    int downSpeed = 0; // 下落速度 每一帧下落的坐标
+    int upStartTime = 0; // 已经跳跃时间
+    int upTime = 10; // 需要跳跃时间
+    int upSpeed = 16; // 每一帧跳跃距离
+    int birdBitmapData = 1;
+    int changeBitmapTime = 0;
+    int BirdSpinData = 0; // 小鸟旋转角度
+    private int gameStatus; // 游戏状态
     private Bitmap bp_bg_day,
             bp_bg_night,
             bp_bird0,
@@ -28,19 +45,38 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
             bp_pipe_up,
             bp_pipe_down,
             bp_panel;
-
     private Canvas mCanvas;
-
     private int ViewWidth, ViewHeight;
     private SurfaceHolder surfaceHolder;
     private boolean isDraw = true; // 绘制线程开关
-
     private Body background;
     private Body bird;
     private Body[] grounds;
     private List<Body> obstacles;
+    private GameListener gameListener;
 
-    int obstacleInterval; // 障碍物上下间距
+    public GameView(Context context) {
+        super(context);
+        initView(context);
+    }
+
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        initView(context);
+    }
+
+    public GameView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        initView(context);
+    }
+
+    public GameListener getGameListener() {
+        return gameListener;
+    }
+
+    public void setGameListener(GameListener gameListener) {
+        this.gameListener = gameListener;
+    }
 
     private void initView(Context context) {
         bp_bg_day = BitmapFactory.decodeResource(context.getResources(), R.drawable.bg_day);
@@ -58,6 +94,8 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
     }
 
     private void initData() {
+        gameStatus = GAME_READY;
+
         ViewWidth = getMeasuredWidth();
         ViewHeight = getMeasuredHeight();
 
@@ -88,39 +126,37 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
         new Thread(this).start(); // 开启绘制线程
     }
 
+//    public void reSet() {
+//        BirdSpinData = 0;
+//        bird.setX(ViewWidth / 2 - bp_bird0.getWidth() / 2);
+//        bird.setY(ViewHeight / 2);
+//        isDown = false;
+//        CreateObstacleTime = 0;
+//        obstacles = new ArrayList<>();
+//        changeBitmapTime = 0;
+//        gameStatus = GAME_READY;
+//        score = 0;
+//
+//        if (gameListener != null) {
+//            gameListener.gameReady();
+//        }
+//    }
+
     private Body[] createGrounds() {
         Body body1 = new Body(bp_land, 0, ViewHeight - bp_land.getHeight(), bp_land.getWidth(), bp_land.getHeight());
         Body body2 = new Body(bp_land, body1.getX() + body1.getW(), ViewHeight - bp_land.getHeight(), bp_land.getWidth(), bp_land.getHeight());
         Body body3 = new Body(bp_land, body2.getX() + body2.getW(), ViewHeight - bp_land.getHeight(), bp_land.getWidth(), bp_land.getHeight());
-        return new Body[] {body1, body2, body3};
+        return new Body[]{body1, body2, body3};
     }
 
     // 获取dx dy缩放比例后的bitmap
     private Bitmap getRatioBitmap(Bitmap bitmap, float dx, float dy) {
         return Bitmap.createScaledBitmap(
                 bitmap,
-                (int)(bitmap.getWidth() * dx),
-                (int)(bitmap.getHeight() * dy),
+                (int) (bitmap.getWidth() * dx),
+                (int) (bitmap.getHeight() * dy),
                 true
         );
-    }
-
-
-
-
-    public GameView(Context context) {
-        super(context);
-        initView(context);
-    }
-
-    public GameView(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        initView(context);
-    }
-
-    public GameView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        initView(context);
     }
 
     //初始化绘图资源
@@ -151,33 +187,111 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
     private void drawMain() { // 绘制整个游戏
         mCanvas = surfaceHolder.lockCanvas();
 
-        birdMove(); // 小鸟飞翔
-        birdDown(); // 小鸟掉落
+        if (gameStatus == GAME_START) {
+            birdMove(); // 小鸟飞翔
+            birdDown(); // 小鸟掉落
+            groundsMove();
+            createObstacle();
+            obstacleMove();
+        }
 
-        groundsMove();
-
-        obstacleMove();
-
-        createObstacle();
-
+        if (gameStatus == GAME_STOP) {
+            overAnim();
+        }
 
         drawBackground(); // 绘制背景
         drawBird(); // 绘制鸟
         drawGrounds(); // 绘制地板
-        drawObstacle();
+        drawObstacle(); // 绘制障碍物
+
+        checkGameOver();
 
         surfaceHolder.unlockCanvasAndPost(mCanvas);
     }
 
-    private void obstacleMove() {
-        for (Body body : obstacles) {
-            body.setX(body.getX() - moveSpeed);
+    private void overAnim() {
+        BirdSpinData += 30;
+
+        if (bird.getX() + bird.getW() / 2 > collisionBody.getX() + collisionBody.getW() / 2) { // 右侧碰撞
+            if (bird.getX() < collisionBody.getX() + collisionBody.getW()) {
+                bird.setX(bird.getX() + 5);
+                bird.setY(bird.getY() - 20);
+                return;
+            }
+            bird.setY(bird.getY() + 20);
+            bird.setX(bird.getX() + 5);
+        } else { // 左侧碰撞
+            if (bird.getX() + bird.getW() > collisionBody.getX()) {
+                bird.setX(bird.getX() - 5);
+                bird.setY(bird.getY() - 20);
+                return;
+            }
+            bird.setY(bird.getY() + 20);
+            bird.setX(bird.getX() - 5);
         }
     }
 
-    int CreateObstacleTime = 0;
+    private void checkGameOver() {
+        if (bird.getY() + bird.getH() >= grounds[0].getY()) {
+            gameStatus = GAME_OVER;
+            if (gameListener != null) {
+                gameListener.gameOver();
+            }
+        } else {
+            for (Body body : obstacles) {
+                if (CollisionRect(bird.getX(), bird.getY(), bird.getW(), bird.getH(),
+                        body.getX(), body.getY(), body.getW(), body.getH())) {
+                    gameStatus = GAME_STOP;
+                    collisionBody = body;
+                }
+            }
+        }
+    }
+
+    public boolean CollisionRect(int x1, int y1, int w1, int h1, int x2, int y2, int w2, int h2) {
+        // 缩小管道的宽度和高度
+        int collisionWidth = w2 - 50;
+        int collisionHeight = h2 - 50;
+
+        // 调整管道的左上角坐标
+        int adjustedX2 = x2 + 25; // 向右移动25像素
+        int adjustedY2 = y2 + 25; // 向下移动25像素
+
+        if (x1 >= adjustedX2 && x1 >= adjustedX2 + collisionWidth) {
+            return false;
+        } else if (x1 <= adjustedX2 && x1 + w1 <= adjustedX2) {
+            return false;
+        } else if (y1 >= adjustedY2 && y1 >= adjustedY2 + collisionHeight) {
+            return false;
+        } else if (y1 <= adjustedY2 && y1 + h1 <= adjustedY2) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void obstacleMove() {
+        List<Body> bodies = new ArrayList<>();
+        for (Body body : obstacles) {
+            body.setX(body.getX() - moveSpeed);
+            if (body.getX() + body.getW() + 10 < 0) {
+                bodies.add(body);
+            }
+
+            if (!body.isScore && body.getX() + body.getW() / 2 < bird.getX()) {
+                score += 1;
+
+                if (gameListener != null) {
+                    gameListener.addScore(score);
+                    body.setScore(true);
+                }
+            }
+        }
+        obstacles.removeAll(bodies);
+    }
+
     private void createObstacle() {
-        CreateObstacleTime ++;
+        CreateObstacleTime++;
         if (CreateObstacleTime >= 100) {
             CreateObstacleTime = 0;
             int ranH = 240 + (int) (Math.random() * (grounds[0].getY() - obstacleInterval - 240)); // 管道最小高度
@@ -236,7 +350,6 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
         }
     }
 
-    int moveSpeed = 10;
     private void groundsMove() {
         // 移动第一个地板
         grounds[0].setX(grounds[0].getX() - moveSpeed);
@@ -267,24 +380,16 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
 
     }
 
-    boolean isDown = true;
-    int downSpeed = 0; // 下落速度 每一帧下落的坐标
-    int upStartTime = 0; // 已经跳跃时间
-    int upTime = 20; // 需要跳跃时间
-    int upSpeed = 12; // 每一帧跳跃距离
-
     private void birdDown() {
         if (isDown) { // 下落
             BirdSpinData = 45;
-            bird.setY(bird.getY() + (downSpeed ++)); // 下落速度递增
-        }
-        else { // 跳跃
+            bird.setY(bird.getY() + (downSpeed++)); // 下落速度递增
+        } else { // 跳跃
             if (upTime - upStartTime >= 0) {
                 BirdSpinData = -45;
                 bird.setY(bird.getY() - upSpeed);
-                upStartTime ++;
-            }
-            else { // 跳跃结束
+                upStartTime++;
+            } else { // 跳跃结束
                 isDown = true;
                 upStartTime = 0;
                 downSpeed = 0;
@@ -292,10 +397,8 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
         }
     }
 
-    int birdBitmapData = 1;
-    int changeBitmapTime = 0;
     private void birdMove() {
-        changeBitmapTime ++;
+        changeBitmapTime++;
         if (changeBitmapTime >= 10) {
             switch (birdBitmapData) {
                 case 1:
@@ -317,7 +420,6 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
         mCanvas.drawBitmap(background.getBitmap(), background.getX(), background.getY(), null);
     }
 
-    int BirdSpinData = 0; // 小鸟旋转角度
     private void drawBird() {
         Matrix matrix = new Matrix();
         matrix.setTranslate(bird.getX(), bird.getY());
@@ -329,18 +431,47 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback, Run
     public boolean onTouchEvent(MotionEvent event) {
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN: // 手指按下
-                isDown = false;
+                if (gameStatus == GAME_READY) {
+                    gameStatus = GAME_START;
+                } else if (gameStatus == GAME_START) {
+                    isDown = false;
+                }
 
                 break;
         }
 
         return super.onTouchEvent(event);
     }
+
+    interface GameListener {
+        void addScore(int score);
+
+        void gameOver();
+
+        void gameReady();
+    }
+
+    public void reSet() {
+        BirdSpinData = 0;
+        bird.setX(ViewWidth / 2 - bp_bird0.getWidth() / 2);
+        bird.setY(ViewHeight / 2);
+        isDown = false;
+        CreateObstacleTime = 0;
+        obstacles = new ArrayList<>();
+        changeBitmapTime = 0;
+        gameStatus = GAME_READY;
+        score = 0;
+
+        gameListener.gameReady();
+
+    }
 }
 
 class Body {
     Bitmap bitmap;
     int x, y, w, h;
+
+    boolean isScore = false;
 
     public Body(Bitmap bitmap, int x, int y, int w, int h) {
         this.bitmap = bitmap;
@@ -350,40 +481,48 @@ class Body {
         this.h = h;
     }
 
+    public boolean isScore() {
+        return isScore;
+    }
+
+    public void setScore(boolean score) {
+        isScore = score;
+    }
+
     public Bitmap getBitmap() {
         return bitmap;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public int getW() {
-        return w;
-    }
-
-    public int getH() {
-        return h;
     }
 
     public void setBitmap(Bitmap bitmap) {
         this.bitmap = bitmap;
     }
 
+    public int getX() {
+        return x;
+    }
+
     public void setX(int x) {
         this.x = x;
+    }
+
+    public int getY() {
+        return y;
     }
 
     public void setY(int y) {
         this.y = y;
     }
 
+    public int getW() {
+        return w;
+    }
+
     public void setW(int w) {
         this.w = w;
+    }
+
+    public int getH() {
+        return h;
     }
 
     public void setH(int h) {
--- app/src/main/java/org/flappybird/MainActivity.java
+++ app/src/main/java/org/flappybird/MainActivity.java
@@ -1,20 +1,24 @@
 package org.flappybird;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    GameView gameView;
+    int Score = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
         getWindow().setFlags(
                 WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -22,6 +26,43 @@ public class MainActivity extends AppCompatActivity {
         );
 
         setContentView(R.layout.activity_main);
+
+        gameView = findViewById(R.id.gameView);
+        gameView.setGameListener(new GameView.GameListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void addScore(final int score) {
+                runOnUiThread(() -> {
+                    Score = score;
+                    ((TextView) findViewById(R.id.score)).setText("Current score: " + score);
+                });
+            }
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void gameOver() {
+                runOnUiThread(() -> {
+                    ((TextView) findViewById(R.id.scoreTxt)).setText("Score:" + Score);
+                    findViewById(R.id.relative).setVisibility(View.VISIBLE);
+                });
+            }
+
+            @Override
+            public void gameReady() {
+                runOnUiThread(() -> findViewById(R.id.relative).setVisibility(View.GONE));
+            }
+        });
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                gameView.reSet();
+                Score = 0; // 重置分数
+                ((TextView) findViewById(R.id.score)).setText("Current score: " + Score); // 更新显示的分数
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
